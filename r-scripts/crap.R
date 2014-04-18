6be4f0740abefdfc8065ec616fafb52b4a79f825
Venkatesh J K <venkatesh.jk@gmail.com>
2014-04-18 21:06:20 +0800

    sectors + performance csv

--- r-scripts/crap.R
+++ r-scripts/crap.R
@@ -1,5 +1,5 @@
 computeEPS <- function(){
-EPS_Growth <- read.csv("~/ba-mashup/csv_data/companies/MARKET/EPS_Growth.csv", header=T)
+EPS_Growth <- read.csv("C:/Deepthi/Distributed-Computing/ba-mashup/csv_data/companies/MARKET/EPS_Growth_New.csv", header=T)
 len=length(EPS_Growth[,'Years'])
 perf<-matrix(data=NA,ncol=2,nrow=500,dimnames=list(c(), c("Symbol", "Performance")))
 as.data.frame(EPS_Growth)
@@ -7,11 +7,16 @@ EPS_Growth[,25:41] <-lapply(EPS_Growth[,25:41], as.double)
 for(row in 1:len){
   sumOfEPS = 0
   perf[row, 'Symbol'] = as.character(EPS_Growth[row, 'Symbol'])
-  print(EPS_Growth[2,'Years'])
-    for(year in 1:EPS_Growth[row,'Years']){
+  years = EPS_Growth[row,'Years']
+  if(years == 0){
+    perf[row, 'Performance'] = as.numeric(0)
+  }else {
+    for(year in 1:years){
       yearValue =  paste('Year', year, sep='')
       sumOfEPS = sum( (sumOfEPS), (EPS_Growth[row, yearValue]))
     }
     perf[row, 'Performance'] = as.numeric(sumOfEPS)/as.numeric(EPS_Growth[row,'Years']);
+  }
 }
+write.csv(perf,"C:/Deepthi/Distributed-Computing/ba-mashup/csv_data/companies/MARKET/CEO_Performance.csv")
 }
